e637a85d1acdd0f01cd08dfd94529f148d19f396
Dattasaikrishna011998 <97234822+Dattasaikrishna011998@users.noreply.github.com>
2022-01-07 15:21:47 +0530

    Create SearchService.java

---
+++ core/src/main/java/com/smarthome/core/services/SearchService.java
@@ -0,0 +1,7 @@
+package com.smarthome.core.services;
+
+import org.json.JSONObject;
+
+public interface SearchService {
+    public JSONObject searchResult(String searchText,int startResult,int resultPerPage);
+}
