467e0c56efbd1efa1dd159983231b71abe5f5f73
Dattasaikrishna011998 <97234822+Dattasaikrishna011998@users.noreply.github.com>
2022-01-07 15:18:12 +0530

    Update Demoservice1.java

--- core/src/main/java/com/smarthome/core/services/Demoservice1.java
+++ core/src/main/java/com/smarthome/core/services/Demoservice1.java
@@ -2,7 +2,7 @@ package com.smarthome.core.services;
 
 import java.util.List;
 
-public interface DemoService1 {
+public interface Demoservice1 {
     public List<String> getPages();
     public String getNameWithReference();
 }
